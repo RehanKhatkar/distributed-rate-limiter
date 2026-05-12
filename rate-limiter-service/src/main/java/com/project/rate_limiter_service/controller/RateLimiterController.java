e21beb98812e2cfd647603f18f2fb98fc2fa1cfe
RehanKhatkar <rehankhatkar2@gmail.com>
2026-05-12 20:32:48 +0530

    Updated controller to return HTTPResponseEntity instead of String

--- rate-limiter-service/src/main/java/com/project/rate_limiter_service/controller/RateLimiterController.java
+++ rate-limiter-service/src/main/java/com/project/rate_limiter_service/controller/RateLimiterController.java
@@ -4,8 +4,12 @@ import com.project.rate_limiter_service.algorithm.RateLimiterStrategy;
 import com.project.rate_limiter_service.metrics.RateLimiterMetrics;
 import com.project.rate_limiter_service.service.RateLimiterStrategyFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 @RestController
 @RequestMapping("/rate-limit")
 @RequiredArgsConstructor
@@ -13,15 +17,26 @@ public class RateLimiterController {
     private final RateLimiterStrategyFactory factory;
     private final RateLimiterMetrics metrics;
     @GetMapping("/{algorithm}")
-    public String test(@PathVariable String algorithm, @RequestHeader("X-Client-Id") String clientId)
-    {
+    public ResponseEntity<Map<String, Object>> test(@PathVariable String algorithm, @RequestHeader("X-Client-Id") String clientId) {
         RateLimiterStrategy strategy = factory.getStrategy(algorithm.toUpperCase());
         boolean allowed = strategy.isAllowed(clientId);
         if (allowed) {
             metrics.incrementAllowed(algorithm);
-            return "Request Allowed";
+            LinkedHashMap<String, Object> response = new LinkedHashMap<>();
+            response.put("status", "success");
+            response.put("message", "Request Allowed");
+            response.put("algorithm", algorithm);
+            return ResponseEntity.ok(response);
+        }else{
+            metrics.incrementBlocked(algorithm);
+            LinkedHashMap<String, Object> response = new LinkedHashMap<>();
+            response.put("status", "blocked");
+            response.put("message", "Rate Limit Exceeded");
+            response.put("algorithm", algorithm);
+
+            return ResponseEntity
+                    .status(HttpStatus.TOO_MANY_REQUESTS)
+                    .body(response);
         }
-        metrics.incrementBlocked(algorithm);
-        return "Rate Limit Exceeded";
     }
 }
